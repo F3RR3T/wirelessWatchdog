5c7aee62e20b7069da46af0119fb811725b08b50
st33v <github@f3rr3t.com>
2017-11-21 07:43:18 +1100

    better verbose logging

--- wlanWDprowl.sh
+++ wlanWDprowl.sh
@@ -27,6 +27,7 @@ echo ${logline} >> ${logdir}/wlanStatus.log
 
 if [ ${wlanStatus} != "routable" ]; then 
     # we have lost wireless, so log the occurrence and restart the service
+    echo "lost wireless" >> ${logdir}/wlanStatus.log
     echo ${logline} >> ${logdir}/wlanStatus.log
     sudo systemctl restart systemd-networkd.service
 fi
